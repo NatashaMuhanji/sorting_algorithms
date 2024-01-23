0af71d7681de1f8b31f50938182038f81e55d55d
NatashaMuhanji <natashamuhanji@gmail.com>
2024-01-23 11:24:41 -0800

    Changes

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -1,13 +1,13 @@
 #include "sort.h"
 /**
- * check_tree - swiftdown check
+ * swift_check - swiftdown check
  * @array: pointer to array
  * @size: size of the pointer
  * @size_init: original size of the array
  * @i: index as a root of the tree
  *
 **/
-void check_tree(int *array, size_t size_init, size_t size, size_t i)
+void swift_check(int *array, size_t size_init, size_t size, size_t i)
 {
 
 	int n, branch1, branch2;
@@ -35,9 +35,9 @@ void check_tree(int *array, size_t size_init, size_t size, size_t i)
 		print_array(array, size_init);
 	}
 	if (br1 < size - 1)
-		check_tree(array, size_init, size, br1);
+		swift_check(array, size_init, size, br1);
 	if (br2 < size - 1)
-		check_tree(array, size_init, size, br2);
+		swift_check(array, size_init, size, br2);
 }
 /**
  * heap_sort - sorts an array of integers
@@ -56,7 +56,7 @@ void heap_sort(int *array, size_t size)
 		return;
 	for (i = 0; i < size / 2 ; i++)
 	{
-		check_tree(array, size_init, size, size / 2 - 1 - i);
+		swift_check(array, size_init, size, size / 2 - 1 - i);
 	}
 	for (i = 0; i < size_init - 1; i++)
 	{
@@ -64,7 +64,7 @@ void heap_sort(int *array, size_t size)
 		array[0] = array[size - 1 - i];
 		array[size - 1 - i] = n;
 		print_array(array, size_init);
-		check_tree(array, size_init, size - i - 1, 0);
+		swift_check(array, size_init, size - i - 1, 0);
 	}
 
 }
